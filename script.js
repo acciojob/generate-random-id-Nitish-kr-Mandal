b928e41041d4483778910f72933ea307d68a31e8
Nitish Kumar Mandal <147975324+Nitish-kr-Mandal@users.noreply.github.com>
2025-11-16 16:37:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
   // write your code here
+	let result = '';
+	let bigString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+	for (let i = 0; i <l; i++) {
+		result+= bigString.charAt(
+			Math.floor(Math.random() * bigString.length)
+		)
+	}
+	return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+const l = prompt("Enter a number.");  
+alert(makeid(l));  
